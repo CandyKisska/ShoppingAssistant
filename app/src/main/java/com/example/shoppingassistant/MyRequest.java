3ff7f728af3fe271ec957f34ed2256a6dd0596a4
Machine831 <yarsem20032@gmail.com>
2020-05-20 16:47:37 +0300

    2

---
+++ app/src/main/java/com/example/shoppingassistant/MyRequest.java
@@ -0,0 +1,26 @@
+package com.example.shoppingassistant;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+
+
+class MyRequest extends AsyncTask<String, Void, Product> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Product doInBackground(String... urls) {
+
+        Log.println(Log.ASSERT, "WORKS",urls[0]);
+        return new EbayRequest().get(urls[1]);
+    }
+
+    @Override
+    protected void onPostExecute(Product result) {
+        super.onPostExecute(result);
+    }
+}
---
+++ app/src/main/java/com/example/shoppingassistant/ProductActivity.java
@@ -0,0 +1,77 @@
+package com.example.shoppingassistant;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ProductActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+
+        Button add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = "Ebay https://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=XML&appid=C-Shopping-PRD-bc51f635b-dd94064c&siteid=77&version=967&ItemID=161933801004&IncludeSelector=Details";
+                Spinner spinner = findViewById(R.id.spinner);
+                Log.println(Log.ASSERT, "Product", "внутри");
+                final EditText href = findViewById(R.id.href);
+                final TextView message = findViewById(R.id.message);
+
+                try {
+                    result = makeRequest(href.getText().toString());
+                    Log.println(Log.ASSERT, "Add", result);
+                    result=spinner.getSelectedItem().toString()+" "+ result;
+                    message.setText("Товар успешно добавлен");
+                    message.setTextColor(Color.GREEN);
+                    Intent intent = new Intent();
+                    intent.putExtra("result", result);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                } catch (Exception e) {
+                    message.setText("Ошибка. Неверная ссылка");
+                    message.setTextColor(Color.RED);
+                }
+
+
+            }
+
+        });
+
+    }
+
+
+    public String makeRequest(String url) throws Exception {
+        String itemId = "";
+        String regex = "https://www.ebay.com/itm/[A-Za-z0-9-]+/[0-9]+[?]+[/A-Za-z0-9_=:%.&�-]*";
+        if (url.matches(regex)) {
+            for (int i = url.indexOf("?") - 1; i > url.indexOf("?") - 20; i--) {
+                if (url.substring(i, i + 1).equals("/")) {
+                    break;
+                } else {
+                    itemId = itemId.concat(url.substring(i, i + 1));
+                }
+            }
+            return "https://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=XML&appid=C-Shopping-PRD-bc51f635b-dd94064c&siteid=0&version=967&ItemID=" + new StringBuffer(itemId).reverse().toString() + "&IncludeSelector=Details";
+
+        } else {
+            throw new Exception();
+        }
+    }
+}
