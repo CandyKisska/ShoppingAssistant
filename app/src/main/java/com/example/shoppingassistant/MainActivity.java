1e12fc0a535e2e0fdd28330dfdf82227a7199b71
Machine831 <yarsem20032@gmail.com>
2020-05-26 15:43:11 +0300

    4

--- app/src/main/java/com/example/shoppingassistant/MainActivity.java
+++ app/src/main/java/com/example/shoppingassistant/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences settings;
     ArrayList<Product> products = new ArrayList<>();
     HashSet<String> urls = new HashSet<>();
+    private Handler mHandler;
+    private int mInterval = 60000;
+    MyRequest myRequest = new MyRequest();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +38,22 @@ public class MainActivity extends AppCompatActivity {
         AndroidThreeTen.init(this);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ProductAdapter productAdapter = new ProductAdapter(products);
+        recyclerView.setAdapter(productAdapter);
 
         settings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         if (settings.contains("Products")) {
             settings.getStringSet("Products", urls);
             Log.println(Log.ASSERT, "List", urls.toString());
-            MyRequest myRequest = new MyRequest();
+
             if (!urls.isEmpty()) {
                 for (String url : urls) {
                     Log.println(Log.ASSERT, "aga", url);
                     myRequest.execute(url);
                     try {
-                        products.add(myRequest.get(5, TimeUnit.SECONDS));
+                        products.add(myRequest.get(10, TimeUnit.SECONDS));
                         recyclerView.getAdapter().notifyDataSetChanged();
 
                     } catch (InterruptedException | ExecutionException | TimeoutException e) {
@@ -58,16 +66,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        recyclerView = findViewById(R.id.list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ProductAdapter titleAdapter = new ProductAdapter(products);
-        recyclerView.setAdapter(titleAdapter);
+        mHandler = new Handler();
+
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
+                stopRepeatingTask();
                 startActivityForResult(intent, 1);
                 Log.println(Log.ASSERT, "execute", "дошло");
 
@@ -80,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        MyRequest myRequest = new MyRequest();
+
         String result = "";
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
@@ -95,14 +102,49 @@ public class MainActivity extends AppCompatActivity {
             myRequest.execute(result);
             try {
                 products.add(myRequest.get(5, TimeUnit.SECONDS));
-             //   reRequest.run(products,recyclerView);
                 recyclerView.getAdapter().notifyDataSetChanged();
+                startRepeatingTask();
 
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
                 e.printStackTrace();
             }
         }
 
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            MyRequest myRequest = new MyRequest();
+            try {
+
+                for (Product product: products){
+                    myRequest.execute(product.url);
+                    products.set(0,myRequest.get(10, TimeUnit.SECONDS));
+                }
+                recyclerView.getAdapter().notifyDataSetChanged();
+
+
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                e.printStackTrace();
+            } finally {
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
     }
 
 }
